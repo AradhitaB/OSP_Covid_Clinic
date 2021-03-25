bcafda83e67036f510eeb8210a93a92f7918fb66
AradhitaB <aradhita02@gmail.com>
2021-03-25 23:17:50 +0530

    frontend changes

--- footer.php
+++ footer.php
@@ -8,23 +8,24 @@
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <h4 class="text-uppercase mb-4">Around the Web</h4>
                         <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/coronavirus_info">
-							facebook</a>
+							<i class="fab fa-fw fa-facebook-f"></i>
+							</a>
 						<a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/covidnewsbymib">
-							twitter</i></a>
+							<i class="fab fa-fw fa-twitter"></i></a>
 						<a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/feed/news/coronavirus-official-updates-4513283">
-							linkedin</i></a>
+							<i class="fab fa-fw fa-linkedin-in"></i></a>
 						<a class="btn btn-outline-light btn-social mx-1" href="https://www.mygov.in/covid-19">
-							government</i></a>
+							<i class="fab fa-fw fa-dribbble"></i></a>
                     </div>
                     <div class="col-lg-4">
                         <h4 class="text-uppercase mb-4">About Us</h4>
-                        <p class="lead mb-0" style="font-size:100%;">This portal was designed for assistance in handling the COVID-19 pandemic by Aradhita, Akshit, and Jeev.</p>
+                        <p class="lead mb-0" style="font-size:175%;">This portal was designed for assistance in handling the COVID-19 pandemic by Aradhita, Akshit, and Jeev.</p>
                     </div>
                 </div>
             </div>
         </footer>
         <div class="copyright py-4 text-center text-white" style="background-color: #007bff; padding: 10px;">
         <div class="wrap">
-			<div class="container"><small>OSP Review 1<br>COVID-19 Telehealth Clinic</small></div>
+			<div class="container">OSP Review 1<br>COVID-19 Telehealth Clinic</div>
         </div>
 		</div>
--- stylehf.php
+++ stylehf.php
@@ -1,6 +1,14 @@
+<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+<script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+<link href="css/styles.css" rel="stylesheet" />
+<link href="css/sb-admin-2.css" rel="stylesheet">
+        
 <style>
   .head{
-    background-image: linear-gradient(#007bff, #e4e6e7);
+    background-image: linear-gradient(#007bff, #6c757d);
     color: white;
     padding: 10px 0px;
     text-shadow: 1px 1px #000000;
@@ -15,6 +23,7 @@
   .navbar a{
 	  color: white;
 	  font-size: 1.5rem;
+	  padding: 10px;
   }
   a:hover{
     color: black;
